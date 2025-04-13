f5bd3e8b9405ea429aad8b3acfd0e4356f0d302a
SewerynKasprzyk <sewkas.studies@gmail.com>
2025-04-13 22:59:00 +0200

    feat(model): added definitions to methods.

--- PO_01/Programowanie_Obiektowe_01/Programowanie_Obiektowe_01/ModelARX.cpp
+++ PO_01/Programowanie_Obiektowe_01/Programowanie_Obiektowe_01/ModelARX.cpp
@@ -1,1 +1,30 @@
 #include "ModelARX.h"
+#include <numeric>
+
+ModelARX::ModelARX(std::vector<double> a, std::vector<double> b, unsigned k, double zmienna)
+{
+	this->a = a;
+	this->b = b;
+	this->k = k;
+
+	// Inicjalizacja buforów
+	this->x.resize(b.size() + this->k, 0.0);
+	this->y.resize(a.size(), 0.0);
+
+}
+
+double ModelARX::symuluj(double u)
+{
+	this->x.push_front(u); // dodaj nowe pobudzenie do bufora wejœciowego
+
+	double ay = std::inner_product(this->a.begin(), this->a.end(), this->y.begin(), 0.0);
+	double bx = std::inner_product(this->b.begin(), this->b.end(), this->x.begin() + this->k, 0.0);
+	double y0 = ay + bx + this->szumTymczasowy();
+
+	this->y.push_front(y0); // dodaj nowe wyjœcie do bufora wyjœciowego
+
+	this->x.pop_back(); // usuñ najstarsze pobudzenie z bufora wejœciowego
+	this->y.pop_back(); // usuñ najstarsze wyjœcie z bufora wyjœciowego
+
+	return y0; // zwróæ nowe wyjœcie
+}
--- PO_01/Programowanie_Obiektowe_01/Programowanie_Obiektowe_01/ModelARX.h
+++ PO_01/Programowanie_Obiektowe_01/Programowanie_Obiektowe_01/ModelARX.h
@@ -1,16 +1,29 @@
 #pragma once
 #include "ObiektSISO.h"
 #include <vector>
-#include <queue>
+#include <deque>
 class ModelARX :
     public ObiektSISO
 {
+public:
+	ModelARX(std::vector<double> a, std::vector<double> b, unsigned k, double zmienna);
+
+	double symuluj(double u) override;
+
+private:
+	//Zmienne
 	std::vector<double> a; // wspó³czynniki a
 	std::vector<double> b; // wspó³czynniki b
 
-	std::queue<double> x; // bufor wejœciowy
-	std::queue<double> y; // bufor wyjœciowy
+	std::deque<double> x; // bufor wejœciowy
+	std::deque<double> y; // bufor wyjœciowy
 
-	unsigned k; // rzêd opóŸnienia
+	unsigned k; // rz¹d opóŸnienia
+
+	//Metoda pomocnicza
+	double szumTymczasowy() 
+	{
+		return 0.0;
+	}
 };
 
